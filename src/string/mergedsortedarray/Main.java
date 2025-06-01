a748cb2ffe1f190bb582f6bc9f1fae0e51673519
hamed miri <hamedmiri@hameds-MacBook-Air.local>
2025-06-01 12:16:33 +0100

    merge sorted array

--- src/string/mergedsortedarray/Main.java
+++ src/string/mergedsortedarray/Main.java
@@ -1,4 +1,21 @@
 package string.mergedsortedarray;
 
+import java.util.Arrays;
+
 public class Main {
+
+    public static void main(String[] args){
+
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+
+        int m = 3;
+        int n = 3;
+
+
+        Solution solution = new Solution();
+        solution.merger(nums1, m, nums2, n);
+
+        System.out.println(Arrays.toString(nums1));
+    }
 }
--- src/string/mergedsortedarray/Solution.java
+++ src/string/mergedsortedarray/Solution.java
@@ -1,4 +1,23 @@
 package string.mergedsortedarray;
 
 public class Solution {
+
+    public void merger(int[] nums1, int m, int[] nums2, int n){
+
+        int midx = m -1;
+        int nidx = n - 1;
+        int right = m + n - 1;
+
+        while(nidx >= 0){
+            if(midx >= 0 && nums2[nidx] > nums1[midx]){
+                nums1[right] = nums2[nidx];
+                nidx--;
+            }else{
+                nums1[right] = nums1[midx];
+                midx--;
+            }
+            right--;
+        }
+
+    }
 }
