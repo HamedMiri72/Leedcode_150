dea8d613c8b77135cdc68762e2b6d8e62b16bed2
hamed miri <hamedmiri@hameds-MacBook-Air.local>
2025-06-15 13:32:24 +0100

    deletion in linked list

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -11,7 +11,18 @@ public class Main {
         linkedList.insertFirstElemnet(5);
         linkedList.insertFirstElemnet(7);
         linkedList.insertLast(16);
+        linkedList.insert(100, 3);
 
+        linkedList.disply();
+
+        System.out.println(linkedList.deleteFirst());
+
+        linkedList.disply();
+
+        System.out.println(linkedList.deleteLast());
+        linkedList.disply();
+
+        System.out.println(linkedList.delete(1));
         linkedList.disply();
 
     }
