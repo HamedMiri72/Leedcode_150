00ba1348cb990ee3fa887153017de15bc81a3f2a
hamedmiri <hamedmiri72@gmail.com>
2025-08-26 13:49:59 +0100

    exception handling

---
+++ src/exceptionHandling/ExceptionHandling.java
@@ -0,0 +1,25 @@
+package exceptionHandling;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args){
+
+        //ArithmeticException
+//        int n = 10;
+//        int m = 0;
+//        int ans = n / m;
+//        System.out.println("Answer: " + ans);
+
+        int n = 10;
+        int m = 0;
+
+        try{
+            int ans = n / m;
+            System.out.println("Ans: " + ans);
+        }catch (ArithmeticException e){
+            System.out.println("Error: Division by zero is not allowed!");
+        }finally {
+            System.out.println("program continue after handling the exception!");
+        }
+    }
+}
